f2f9efccbe5d087eb184ed38406e8807a18f5c29
arunbabu6 <153723482+arunbabu6@users.noreply.github.com>
2024-04-02 14:58:40 +0100

    Update and rename jest.config.js to je

--- client/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    collectCoverage: true, // Enable coverage collection
-    coverageReporters: ["lcov", "text"], // Specify coverage reporters
-    // Optional: Specify the directory where Jest should output its coverage files
-    coverageDirectory: "coverage",
-};
